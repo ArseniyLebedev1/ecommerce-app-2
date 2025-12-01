c5045936bdff4af7e00ae080bca985cdfb7493c0
ArseniyLebedev1 <imarseniylebedev@gmail.com>
2025-12-01 15:30:17 +0300

    setup GitHub Pages deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ecommerce-app-2/products">
       <App />
     </BrowserRouter>
   </StrictMode>
