734e69ea98d7281580f8e36287ffd3dc0c574898
ArseniyLebedev1 <imarseniylebedev@gmail.com>
2025-12-01 15:41:23 +0300

    deploy settings changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/ecommerce-app-2/products">
+    <BrowserRouter basename="/ecommerce-app-2">
       <App />
     </BrowserRouter>
   </StrictMode>
